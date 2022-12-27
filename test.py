72bc1bcee07cedbeaa116bef1b3200d7afd900db
pepsakdoek <65312601+pepsakdoek@users.noreply.github.com>
2022-12-27 17:17:00 +0200

    Fix bug on empty charcombos, and uppercase the compare char

--- test.py
+++ test.py
@@ -4,6 +4,7 @@ from datetime import datetime
 # tests and usage
 a = t.readdict("Woorde.csv")
 e = t.readdict("words_alpha.txt")
+w = t.readdict("wordleanswerlist.txt")
 timestart = datetime.now()
 
 
